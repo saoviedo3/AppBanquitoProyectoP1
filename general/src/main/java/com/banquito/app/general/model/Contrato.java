81727148b0d9a8d4c0c493a23d98dd4c6e8f9946
saoviedo3 <78757400+saoviedo3@users.noreply.github.com>
2025-05-23 00:47:59 -0500

    BasePrograma
    Se sube la base del programa

---
+++ general/src/main/java/com/banquito/app/general/model/Contrato.java
@@ -0,0 +1,10 @@
+package com.banquito.app.general.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Contratos")
+public class Contrato {
+
+}
---
+++ general/src/main/java/com/banquito/app/general/model/HistorialEstadoId.java
@@ -0,0 +1,8 @@
+package com.banquito.app.general.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class HistorialEstadoId {
+
+}
