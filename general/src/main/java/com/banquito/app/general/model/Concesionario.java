57469fea2f9160dcd6d46c6780fddb53a7fcb3d0
saoviedo3 <78757400+saoviedo3@users.noreply.github.com>
2025-05-23 20:51:24 -0500

    capa model
    Se crea la capa model y sus relaciones

---
+++ general/src/main/java/com/banquito/app/general/model/Concesionario.java
@@ -0,0 +1,24 @@
+package com.banquito.app.general.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Concesionarios")
+public class Concesionario {
+
+    @OneToMany(mappedBy = "concesionario")
+    private List<Vehiculo> vehiculos;
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+}
